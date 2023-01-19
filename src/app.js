bebeea0e69acf01baf77b05c946d41ae62a61bd4
Isabela Telles <itellesdelira@gmail.com>
2023-01-19 20:15:01 -0300

    add coisas

--- src/app.js
+++ src/app.js
@@ -1,23 +1,25 @@
-import express from 'express'
-import { pool } from './db.js'
-import {PORT} from './config.js'
+import express from "express";
+import { pool } from "./db.js";
+import { PORT } from "./config.js";
 
-const app = express()
+const app = express();
 
-app.get('/', async (req, res) => {
-  const [rows] = await pool.query('SELECT * FROM users')
-  res.json(rows)
-})
+app.get("/", async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM users");
+  res.json(rows);
+});
 
-app.get('/ping', async (req, res) => {
+app.get("/ping", async (req, res) => {
   const [result] = await pool.query(`SELECT "hello world" as RESULT`);
-  res.json(result[0])
-})
+  res.json(result[0]);
+});
 
-app.get('/create', async (req, res) => {
-  const result = await pool.query('INSERT INTO users(name) VALUES ("John")')
-  res.json(result)
-})
+app.post("/create", async (req, res) => {
+  let name = req.body.name;
+  const result = await pool.query(`INSERT INTO users(name) VALUES (${name})`);
 
-app.listen(PORT)
-console.log('Server on port', PORT)
+  res.status(201).json(result);
+});
+
+app.listen(PORT);
+console.log("Server on port", PORT);
