0850293e82f9f7e40ab6ac7602361f6fe1496f42
Isabela Telles <itellesdelira@gmail.com>
2023-01-19 20:24:18 -0300

    ??

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.get("/ping", async (req, res) => {
 
 app.post("/create", async (req, res) => {
   const name = req.body.name;
-  const result = await pool.query("INSERT INTO users(name) VALUES (?)", [name]);
+  const result = await pool.query("INSERT INTO users(name) VALUES (?)", name);
 
   res.status(201).json(result);
 });
