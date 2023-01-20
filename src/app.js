094632b90f509613520e65a7265198b23251c983
Isabela Telles <itellesdelira@gmail.com>
2023-01-20 19:33:12 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -81,5 +81,44 @@ app.post("/criarDoador", async (req, res) => {
   res.json([result]);
 });
 
+app.post("/cadastrarDoacao", async (req, res) => {
+  const nome = req.body.nome;
+  const end = req.body.endereco;
+  const num = req.body.numero;
+  const bairro = req.body.bairro;
+  const cep = req.body.cep;
+  const cidade = req.body.cidade;
+  const uf = req.body.uf;
+  const tel = req.body.telefone;
+  const vac = req.body.vacina;
+  const idade = req.body.idade;
+  const tipoSanguineo = req.body.tipoSanguineo;
+  const sexo = req.body.sexo;
+  const doenca = req.body.doenca;
+
+  console.log(tipoSanguineo);
+
+  const result = await pool.query(
+    "INSERT INTO Doadores(nome, endereco, numero, bairro, cep, cidade, uf, telefone, vacina, idade, tipoSanguineo, sexo, doenca) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ?, ?)",
+    [
+      nome,
+      end,
+      num,
+      bairro,
+      cep,
+      cidade,
+      uf,
+      tel,
+      vac,
+      idade,
+      tipoSanguineo,
+      sexo,
+      doenca,
+    ]
+  );
+
+  res.json([result]);
+});
+
 app.listen(PORT);
 console.log("Server on port", PORT);
