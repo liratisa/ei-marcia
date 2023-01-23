7b1a3b0413f1f48aa6cd73474de5bad50e15d08a
Isabela Telles <itellesdelira@gmail.com>
2023-01-23 13:58:08 -0300

    add rota delete

--- src/app.js
+++ src/app.js
@@ -94,5 +94,15 @@ app.post("/cadastrarDoacao", async (req, res) => {
   res.json(result);
 });
 
+app.delete("/doadores/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const result = await pool.query("DELETE FROM Doadores WHERE doadorID = ?", [
+    id,
+  ]);
+
+  res.json(result);
+});
+
 app.listen(PORT);
 console.log("Server on port", PORT);
